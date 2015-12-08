3499dee48ac692803ad34e427a801c6df36ab5a7
arbuuuud <arief.setiabudi2010@gmail.com>
2015-12-08 16:43:42 +0700

    Revert "Revert "add database local""
    This reverts commit c70e16b06a928f7f8fe7f1928075339fd0fef3f4.

--- app/config/database.php
+++ app/config/database.php
@@ -55,9 +55,9 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => '127.0.0.1',
-			'database'  => 'mpr',
+			'database'  => 'aops',
 			'username'  => 'root',
-			'password'  => 'password',
+			'password'  => '',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
