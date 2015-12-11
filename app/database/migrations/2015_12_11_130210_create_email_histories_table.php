e7691579ad460be4bdfd432676228e027c2ee9d1
arbuuuud <arief.setiabudi2010@gmail.com>
2015-12-11 13:03:47 +0700

    Initaial Generate Scaffold
    still empty class

---
+++ app/database/migrations/2015_12_11_130210_create_email_histories_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateEmailHistoriesTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('email_histories', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->integer('template_id');
+			$table->integer('member_id');
+			$table->integer('contact_id');
+			$table->datetime('date_sent');
+			$table->integer('is_automatic');
+			$table->timestamps();
+		});
+	}
+
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('email_histories');
+	}
+
+}
---
+++ app/models/EmailHistory.php
@@ -0,0 +1,5 @@
+<?php
+
+class EmailHistory extends \Eloquent {
+	protected $fillable = [];
+}
