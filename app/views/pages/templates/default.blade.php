b55e6fa24e5ef92aa0d44b4967acf7c99c9c6cca
indroputranto <indro.putranto@asclar.com>
2016-04-20 13:11:45 +0700

    minor repairs 20.04.16

--- app/views/pages/templates/default.blade.php
+++ app/views/pages/templates/default.blade.php
@@ -5,6 +5,34 @@
 @stop
 
 @section('content')
+
+<div id="menu-popup">
+    <ul id="menu-popup-items">
+        <li><a href="{{url('/')}}">Home</a></li>
+        <li><a href="{{url('/kategori/uncategorized')}}">Article</a></li>
+    </ul>
+    <div id="menu-popup-close">
+        X
+    </div>
+</div>
+
+<div id="article-top-bar" class="container-fluid">
+    <div class="row">
+        <div class="col-sm-11 col-xs-8">
+          <a href="{{url('/')}}">{{ HTML::image( 'images/logo-2.png', 'logo-image', array( 'class' => 'logo-image' ) ) }}</a>
+        </div>
+        <div id="nav-menu" class="col-sm-1 col-xs-4">
+            <div class="menu-text">
+                Menu
+            </div>
+            <div class="menu-icon">
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
 <div class="container">
     <div class="row">
     	<div class="col-md-12">
